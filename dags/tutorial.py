94c7ed00995c74a6d6556bc0ba204763614acf66
Rhyando <88094242+kurangdoa@users.noreply.github.com>
2023-03-21 09:36:04 +0100

    Update tutorial.py

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -1,26 +1,25 @@
-from airflow.decorators import dag, task
+import datetime
+
 import pendulum
 
+from airflow.models.dag import DAG
+from airflow.operators.empty import EmptyOperator
 
-@dag(
-    schedule_interval=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz='UTC'),
+now = pendulum.now(tz="UTC")
+now_to_the_hour = (now - datetime.timedelta(0, 0, 0, 0, 0, 3)).replace(minute=0, second=0, microsecond=0)
+START_DATE = now_to_the_hour
+DAG_NAME = "test_dag_gitsync"
+
+dag = DAG(
+    DAG_NAME,
+    schedule="*/10 * * * *",
+    default_args={"depends_on_past": True},
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=['sample']
 )
-def sample_etl():
-    @task()
-    def extract():
-        pass
 
-    @task()
-    def transform():
-        pass
-
-    @task()
-    def load():
-        pass
-
-    extract() >> transform() >> load()
-
-sample_dag = sample_etl()
+run_this_1 = EmptyOperator(task_id="run_this_1", dag=dag)
+run_this_2 = EmptyOperator(task_id="run_this_2", dag=dag)
+run_this_2.set_upstream(run_this_1)
+run_this_3 = EmptyOperator(task_id="run_this_3", dag=dag)
+run_this_3.set_upstream(run_this_2)
