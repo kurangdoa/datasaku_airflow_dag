98f880c72a51c07fa7c283db4c6146af707acf45
kurangdoa <rhyando.anggoro-adi@brenntag.com>
2023-03-30 22:04:16 +0200

    init

--- dags/scripts/test_print_script.py
+++ dags/scripts/test_print_script.py
@@ -4,5 +4,5 @@ import logging
 start_date = pendulum.datetime(2023, 1, 1)
 end_date = pendulum.datetime(2023, 1, 2)
 
-logging.getLogger("airflow.task")
-logging.info("end test")
+logger = logging.getLogger("airflow.task")
+logger.info("end test")
