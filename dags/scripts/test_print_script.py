db7f847e0cee606853ca709dd5f5698b503f1196
kurangdoa <rhyando.anggoro-adi@brenntag.com>
2023-03-31 22:43:41 +0200

    fix

--- dags/scripts/test_print_script.py
+++ dags/scripts/test_print_script.py
@@ -1,15 +1,8 @@
 import pendulum
-import logging
 from airflow.utils.log.logging_mixin import LoggingMixin
 
 start_date = pendulum.datetime(2023, 1, 1)
 end_date = pendulum.datetime(2023, 1, 2)
-
-logger = logging.getLogger("airflow.task")
-logger.info("start from logger")
-logger.info("start_date: %s", start_date)
-logger.info("end_date: %s", end_date)
-logger.info("end from logger")
 
 LoggingMixin().log.info("start from LoggingMixin")
 LoggingMixin().log.info("start_date: %s", start_date)
