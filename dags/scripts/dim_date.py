f9b3d845110556d867c3817dfbff86a2de13cc9a
kurangdoa <rhyando.anggoro-adi@brenntag.com>
2023-04-16 21:03:59 +0200

    fix

--- dags/scripts/dim_date.py
+++ dags/scripts/dim_date.py
@@ -443,38 +443,38 @@ LoggingMixin().log.info(aws_key)
 LoggingMixin().log.info("sample dataset: ")
 LoggingMixin().log.info(df.head())
 
-# boto = datasaku_s3.ConnS3(aws_access_key_id = aws_secret, aws_secret_access_key = aws_key, aws_session_token=None)
+boto = datasaku_s3.ConnS3(aws_access_key_id = aws_secret, aws_secret_access_key = aws_key, aws_session_token=None)
 
-# # upload the file
-# boto.s3_upload_file('datasaku', 'self_generated_data/dim_date.parquet', df)
+# upload the file
+boto.s3_upload_file('datasaku', 'self_generated_data/dim_date.parquet', df)
 
-class ConnS3:
-    """Class for S3"""
-    def __init__(self, aws_access_key_id, aws_secret_access_key, aws_session_token = None):
-        self.aws_id =  aws_access_key_id
-        self.aws_key = aws_secret_access_key
-        self.aws_token = aws_session_token
+# class ConnS3:
+#     """Class for S3"""
+#     def __init__(self, aws_access_key_id, aws_secret_access_key, aws_session_token = None):
+#         self.aws_id =  aws_access_key_id
+#         self.aws_key = aws_secret_access_key
+#         self.aws_token = aws_session_token
 
-        # create session
-        if (self.aws_id is not None) & (self.aws_key is not None):
-            self.s3_session = boto3.Session(
-                aws_access_key_id = self.aws_id,
-                aws_secret_access_key = self.aws_key,
-                aws_session_token = self.aws_token,
-            ) # create aws session if there is aws credential defined
-        else:
-            self.s3_session = boto3.Session() # create aws session using using environment detail 
+#         # create session
+#         if (self.aws_id is not None) & (self.aws_key is not None):
+#             self.s3_session = boto3.Session(
+#                 aws_access_key_id = self.aws_id,
+#                 aws_secret_access_key = self.aws_key,
+#                 aws_session_token = self.aws_token,
+#             ) # create aws session if there is aws credential defined
+#         else:
+#             self.s3_session = boto3.Session() # create aws session using using environment detail 
 
-    def s3_list_bucket(self):
-        """list bucket inside s3"""
-        s3_resource = self.s3_session.resource('s3') # create s3 resource
-        buckets = [bucket.name for bucket in s3_resource.buckets.all()] # list down the bucket
-        return buckets
+#     def s3_list_bucket(self):
+#         """list bucket inside s3"""
+#         s3_resource = self.s3_session.resource('s3') # create s3 resource
+#         buckets = [bucket.name for bucket in s3_resource.buckets.all()] # list down the bucket
+#         return buckets
 
 
-# conn = BaseHook.get_connection('aws_default')
-# LoggingMixin().log.info(conn.get_extra())
+# # conn = BaseHook.get_connection('aws_default')
+# # LoggingMixin().log.info(conn.get_extra())
 
-boto = ConnS3(aws_access_key_id = aws_secret, aws_secret_access_key = aws_key)
-my_bucket = boto.s3_list_bucket()
-LoggingMixin().log.info(my_bucket)
+# boto = ConnS3(aws_access_key_id = aws_secret, aws_secret_access_key = aws_key)
+# my_bucket = boto.s3_list_bucket()
+# LoggingMixin().log.info(my_bucket)
