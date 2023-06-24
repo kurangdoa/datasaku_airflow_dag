5dc2e92c1230079dab6f2154c4c381dc0f54e03a
kurangdoa <rhyando.anggoro-adi@brenntag.com>
2023-06-24 14:36:13 +0200

    fix sysappend

--- dags/samsung/scripts/transformation_bronze.py
+++ dags/samsung/scripts/transformation_bronze.py
@@ -3,9 +3,16 @@ sys.path.insert(1, '/opt/airflow/dags/repo/dags/samsung/')
 import utils.kaggle as kaggle
 import pandas as pd
 from airflow.models import Variable
+import os
 
-kaggle = kaggle.kaggle_api(Variable.get("KAGGLE_USERNAME"), Variable.get("KAGGLE_KEY"))
-kaggle.download_dataset('lipann/prepaired-data-of-customer-revenue-prediction')
+os.environ['KAGGLE_USERNAME'] = Variable.get("KAGGLE_USERNAME")
+os.environ['KAGGLE_KEY'] = Variable.get("KAGGLE_KEY")
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+api.dataset_download_files('lipann/prepaired-data-of-customer-revenue-prediction', unzip=True)
 
 # moda means the categorical feature stated as integer 
 bronze_train_categorical_features_moda = pd.read_csv('train_categorial_features_moda.csv')
