4846ef82798309e90e12fb76dc6e3680ae81d773
kurangdoa <rhyando.anggoro-adi@brenntag.com>
2023-06-24 15:00:31 +0200

    fix sysappend

--- dags/samsung/scripts/transformation_bronze.py
+++ dags/samsung/scripts/transformation_bronze.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.insert(1, '/opt/airflow/dags/repo/dags/samsung/')
+import utils.datasaku_sqlalchemy as datasaku_sqlalchemy
 import pandas as pd
 from airflow.models import Variable
+from kaggle.api.kaggle_api_extended import KaggleApi
 import os
 
+# connection to kaggle
 os.environ['KAGGLE_USERNAME'] = Variable.get("KAGGLE_USERNAME")
 os.environ['KAGGLE_KEY'] = Variable.get("KAGGLE_KEY")
-
-from kaggle.api.kaggle_api_extended import KaggleApi
-
 api = KaggleApi()
 api.authenticate()
 api.dataset_download_files('lipann/prepaired-data-of-customer-revenue-prediction', unzip=True)
@@ -16,3 +16,50 @@ api.dataset_download_files('lipann/prepaired-data-of-customer-revenue-prediction
 # moda means the categorical feature stated as integer 
 bronze_train_categorical_features_moda = pd.read_csv('train_categorial_features_moda.csv')
 print(bronze_train_categorical_features_moda.head())
+
+# train_flat
+train_flat = pd.read_csv('train_flat.csv')
+
+##### bronze layer #####
+
+# Bronze layer is the layer as close as possible to the raw data for troubleshooting purpose
+# Schema adjustment is done in Bronze layer as well to match organization fit.
+
+fct_bronze_google_analytics = train_flat.copy()
+
+# based on data provided, there are two main dataset, flat and filtered
+# filtered dataset <> flat dataset (with filter) -- because there are records in filtered dataset that not exist in flat dataset
+# for example 7530866178634633311
+# decided to use flat dataset as main dataset because of long term reason of column availability compared to filtered dataset such as 
+#  'sessionId',
+#  'socialEngagementType',
+#  'visitId',
+#  'totals_visits',
+#  'device_browserSize',
+#  'device_browserVersion',
+#  'device_flashVersion',
+#  'device_language',
+#  'device_mobileDeviceBranding',
+#  'device_mobileDeviceInfo',
+#  'device_mobileDeviceMarketingName',
+#  'device_mobileDeviceModel',
+#  'device_mobileInputSelector',
+#  'device_operatingSystemVersion',
+#  'device_screenColors',
+#  'device_screenResolution',
+#  'geoNetwork_cityId',
+#  'geoNetwork_latitude',
+#  'geoNetwork_longitude',
+#  'geoNetwork_networkLocation',
+#  'trafficSource_adwordsClickInfo.criteriaParameters',
+#  'trafficSource_adwordsClickInfo.gclId',
+#  'trafficSource_campaignCode'
+
+##### save to sql #####
+samsung = datasaku_sqlalchemy.sqlalchemy_class(username = 'postgres', port = 5555)
+samsung.execute_create_database('samsung')
+samsung = datasaku_sqlalchemy.sqlalchemy_class(username = 'postgres', port = 5555, database = 'samsung')
+samsung.pandas_to_sql(df = fct_bronze_google_analytics, table_name = 'fct_bronze_google_analytics', schema_name = 'bronze', if_exists = 'replace')
+test = samsung.sql_to_pandas("""SELECT * FROM bronze.fct_bronze_google_analytics LIMIT 5;""")
+print(test.head())
+
---
+++ dags/samsung/utils/datasaku_sqlalchemy.py
@@ -0,0 +1,67 @@
+import sqlalchemy
+from sqlalchemy.sql import text
+
+class sqlalchemy_class():
+    def __init__(self, username, password=None, host='localhost', port=5432, database='postgres'):
+        self.sqlalchemy_username = username
+        self.sqlalchemy_password = password
+        self.sqlalchemy_host = host
+        self.sqlalchemy_port = port
+        self.sqlalchemy_database = database
+        self.engine = sqlalchemy.create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
+
+    def show(self):
+        print("username is", self.sqlalchemy_username)
+        # print("password is", self.sqlalchemy_password)
+        print("host is", self.sqlalchemy_host)
+        print("port is", self.sqlalchemy_port)
+        print("database is", self.sqlalchemy_database)
+
+    def pandas_to_sql(self, df, table_name, schema_name, if_exists='fail'):
+        try:
+            check = df.to_sql(table_name, con=self.engine, if_exists=if_exists, index= False, schema = schema_name)
+            if check == None:
+                info = "Table creation failed"
+            else:
+                info = "Table creation success"
+        except:
+            info = "Table creation failed"
+        return print(info)
+    
+    def execute_create_database(self, database_name):
+        try:
+            with self.engine.connect() as conn:
+                sql = f"""CREATE DATABASE {database_name}"""
+                conn.execution_options(isolation_level="AUTOCOMMIT").execute(text(sql))
+                info = "Database created successfully"
+        except:
+            info = "Database creation failed"
+        return print(info)
+    
+    def execute_drop_database(self, database_name):
+        try:
+            with self.engine.connect() as conn:
+                sql = f"""DROP DATABASE IF EXISTS {database_name};"""
+                conn.execution_options(isolation_level="AUTOCOMMIT").execute(text(sql))
+                info = "Database deleted successfully"
+        except:
+            info = "Database deletion failed"
+        return print(info)
+
+    def execute_query(self, query):
+        try:
+            with self.engine.connect() as conn:
+                conn.execute(query)
+                info ="Query executed successfully"
+        except:
+            info = "Query execution failed"
+        return print(info)
+
+    def sql_to_pandas(self, query):
+        try:
+            with self.engine.connect() as conn:
+                query = conn.execute(text(query))         
+            df = pd.DataFrame(query.fetchall())
+        except:
+            print('Query execution failed')
+        return df
